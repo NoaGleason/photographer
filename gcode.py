7a2ce985934c0e1c5889e4b13901fb93a792f2cf
Noah Gleason <noah.gleason1@gmail.com>
2020-12-03 19:33:12 -0800

    Update for V2

--- gcode.py
+++ gcode.py
@@ -1,7 +1,11 @@
 
 with open("tour.gcode", "w") as file:
+    start_x = 8
+    start_y = 2
     file.write("G90\n")
-    file.write("G0X0Y0\n")
+    file.write("$HX\n")
+    file.write("$HY\n")
+    file.write("G0X{}Y{}\n".format(start_x, start_y))
     file.write("S1\n")
     file.write("F1000\n")
     for row in range(15):
@@ -10,7 +14,7 @@ with open("tour.gcode", "w") as file:
         else:
             order = range(19, -1, -1)
         for col in order:
-            file.write("G01X{}Y{}\n".format(1.5+col*5, row*-2.5))
+            file.write("G01X{}Y{}\n".format(start_x + col * 4.5, start_y - row * 4.5))
             file.write("G04P0.25\n")
             file.write("M3\n")
             file.write("G04P0.25\n")
--- photographer.py
+++ photographer.py
@@ -8,37 +8,37 @@ with open("config.json", "r") as file:
 index = 0
 flag = False
 
-V4L_TO_CV2_NAMES = {"Brightness": cv2.CAP_PROP_BRIGHTNESS, "Contrast": cv2.CAP_PROP_CONTRAST,
-                    "Saturation": cv2.CAP_PROP_SATURATION, "Hue": cv2.CAP_PROP_HUE,
-                    "White Balance Temperature, Auto": cv2.CAP_PROP_AUTO_WB, "Gamma": cv2.CAP_PROP_GAMMA,
-                    "Gain": cv2.CAP_PROP_GAIN, "White Balance Temperature": cv2.CAP_PROP_WB_TEMPERATURE,
-                    "Sharpness": cv2.CAP_PROP_SHARPNESS, "Backlight Compensation": cv2.CAP_PROP_BACKLIGHT,
-                    "Exposure, Auto": cv2.CAP_PROP_AUTO_EXPOSURE, "Exposure (Absolute)": cv2.CAP_PROP_EXPOSURE}
+V4L_TO_CV2_NAMES = {"brightness": cv2.CAP_PROP_BRIGHTNESS, "contrast": cv2.CAP_PROP_CONTRAST,
+                    "saturation": cv2.CAP_PROP_SATURATION, "hue": cv2.CAP_PROP_HUE,
+                    "white_balance_temperature_auto": cv2.CAP_PROP_AUTO_WB, "gamma": cv2.CAP_PROP_GAMMA,
+                    "gain": cv2.CAP_PROP_GAIN, "white_balance_temperature": cv2.CAP_PROP_WB_TEMPERATURE,
+                    "sharpness": cv2.CAP_PROP_SHARPNESS, "backlight_compensation": cv2.CAP_PROP_BACKLIGHT,
+                    "exposure_auto": cv2.CAP_PROP_AUTO_EXPOSURE, "exposure_absolute": cv2.CAP_PROP_EXPOSURE}
 
 
 def set_camera_settings(filename: str, capture: cv2.VideoCapture):
     with open(filename, "r") as f:
         for line in f:
-            parts = line.split(":")
-            if len(parts) == 3 and parts[1] in V4L_TO_CV2_NAMES:
-                capture.set(V4L_TO_CV2_NAMES[parts[1]], int(parts[2]))
+            prop_name = line.split(" ")[0].strip()
+            prop_val = line.split("=")[-1].strip()
+            if prop_name in V4L_TO_CV2_NAMES:
+                capture.set(V4L_TO_CV2_NAMES[prop_name], int(prop_val))
 
 
 cap = cv2.VideoCapture(int(config["camera"]))
 set_camera_settings("camera-settings.txt", cap)
 
-serial_connection = serial.Serial(config["serial"], '115200', parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE)
-# serial_connection.open()
-
-while True:
-    recv = serial_connection.read(1)
-    letter = chr(recv[0])
-    if letter.isdecimal() and letter != "0":
-        if not flag:
-            print("picture time")
-            return_value, image = cap.read()
-            cv2.imwrite("well_pics/{:03d}.png".format(index), image)
-            index += 1
-            flag = True
-    elif flag and letter.isdecimal():
-        flag = False
+with serial.Serial(config["serial"], '115200', parity=serial.PARITY_NONE, stopbits=serial.STOPBITS_ONE) as serial_connection:
+    print("connected")
+    while True:
+        recv = serial_connection.read(1)
+        letter = chr(recv[0])
+        if letter.isdecimal() and letter != "0":
+            if not flag:
+                print("picture time")
+                return_value, image = cap.read()
+                cv2.imwrite("./well_pics/{:03d}.png".format(index), image)
+                index += 1
+                flag = True
+        elif flag and letter.isdecimal():
+            flag = False
